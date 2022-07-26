8c98a2bb302fe160d5ae7e08cba43d380f385288
LuisZMarques <2212522@my.ipleiria.pt>
2022-07-26 22:53:31 +0100

    zcode

--- index.js
+++ index.js
@@ -1,53 +1,50 @@
-let num = 0;
-
 // Input value & converter button selectors
-const inputValue = document.getElementById('value-input')
+
+const inputValue = document.getElementById('value-input').innerHTML
 const inputButton = document.getElementById('input-btn')
 
 // unit button selectors for page title
-
-const metricBtn = document.getElementById('metric-btn')
-const LitersBtn = document.getElementById('liters-btn')
-const kilosBtn = document.getElementById('kilograms-btn')
+var foo = 1
 let title = document.getElementById('title-converter')
 
-metricBtn.addEventListener('click', () => {
-    title.textContent = 'METROSCRL'
+const btns = document.querySelectorAll('button[id^=btn]')
+
+const displayValue = document.getElementById('display-value')
+const displayName = document.getElementById('display-name')
+
+btns.forEach(btn =>{
+    btn.addEventListener('click', event => {
+        document.getElementById('title-converter').innerText = btn.textContent + " Converter"
+        inputButton.addEventListener('click',
+        function (){
+            if(btn.textContent === 'Meters'){
+                console.log(inputValue)
+                console.log(inputValue.innerText)
+                foo = (inputValue * 3.28084).toFixed(2)
+                displayValue.innerText = foo
+                displayName.innerText = "Meters To Feet"
+                console.log(displayValue.innerHTML)
+            }else if(btn.textContent === 'Liters'){
+                console.log(inputValue)
+                console.log(inputValue.innerText)
+                displayValue.innerText = (inputValue * 0.264).toFixed(2)
+                displayName.innerText = "Liters to Gallons"
+                console.log(displayName.innerText)
+                console.log(btn.textContent)
+                console.log(displayValue.innerText)
+            }else{
+                console.log(inputValue)
+                console.log(inputValue.innerText)
+                displayValue.innerText = (inputValue * 2.204).toFixed(2)
+                displayName.innerText = "Kilos To Pounds"
+                console.log(displayName.innerText)
+                console.log(btn.textContent)
+                console.log(displayValue.innerText)
+            }
+        })
+    
+    })
 })
-
-LitersBtn.addEventListener('click', () => {
-    title.textContent = 'LITROS'
-})
-
-kilosBtn.addEventListener('click', () => {
-    title.textContent = 'KILOGRAMS'
-})
-
-
-
-
-// paragraph for displaying the value results to strings.
-
-// metricEl.addEventListener('click', function () {
-//     const selectBtn = document.getElementById('metric-btn').innerText
-//     document.getElementById('title-converter').innerText = selectBtn
-
-// })
-
-
-// const lengthEl = document.getElementById('length')
-// const litersEl = document.getElementById('liters')
-// const kilosEl = document.getElementById('kilos')
-
-inputButton.addEventListener('click'),
-    function () {
-        num = (inputValue * 3.28084).toFixed(2);
-        num = (inputValue * 0.3048).toFixed(2);
-        console.log(num);
-        lengthEl.textContent = `${inputValue} meters =  `
-    }
-
-
 /*
 1 meter = 3.281 feet
 1 liter = 0.264 gallon
