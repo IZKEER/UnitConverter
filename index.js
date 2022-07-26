ed52736b983c11f5b0deb26c14985ae2b7fd14c2
LuisZMarques <2212522@my.ipleiria.pt>
2022-07-26 23:57:46 +0100

    completed with getelementbyid.value

--- index.js
+++ index.js
@@ -1,45 +1,29 @@
 // Input value & converter button selectors
-
-const inputValue = document.getElementById('value-input').innerHTML
-const inputButton = document.getElementById('input-btn')
-
+let inputValue;
+let inputButton = document.getElementById('input-btn')
 // unit button selectors for page title
-var foo = 1
 let title = document.getElementById('title-converter')
-
-const btns = document.querySelectorAll('button[id^=btn]')
-
-const displayValue = document.getElementById('display-value')
-const displayName = document.getElementById('display-name')
+let btns = document.querySelectorAll('button[id^=btn]')
+let displayValue = document.getElementById('display-value')
+let displayName = document.getElementById('display-name')
 
 btns.forEach(btn =>{
     btn.addEventListener('click', event => {
         document.getElementById('title-converter').innerText = btn.textContent + " Converter"
         inputButton.addEventListener('click',
         function (){
+            inputValue = document.getElementById('value').value
+            console.log(inputValue)
             if(btn.textContent === 'Meters'){
-                console.log(inputValue)
-                console.log(inputValue.innerText)
-                foo = (inputValue * 3.28084).toFixed(2)
-                displayValue.innerText = foo
                 displayName.innerText = "Meters To Feet"
-                console.log(displayValue.innerHTML)
+                displayValue.innerText = (inputValue * 3.28084).toFixed(2);
             }else if(btn.textContent === 'Liters'){
-                console.log(inputValue)
-                console.log(inputValue.innerText)
-                displayValue.innerText = (inputValue * 0.264).toFixed(2)
                 displayName.innerText = "Liters to Gallons"
-                console.log(displayName.innerText)
-                console.log(btn.textContent)
-                console.log(displayValue.innerText)
+                displayValue.innerText = (inputValue * 0.264).toFixed(2);
             }else{
-                console.log(inputValue)
-                console.log(inputValue.innerText)
-                displayValue.innerText = (inputValue * 2.204).toFixed(2)
                 displayName.innerText = "Kilos To Pounds"
-                console.log(displayName.innerText)
-                console.log(btn.textContent)
-                console.log(displayValue.innerText)
+                displayValue.innerText = (inputValue * 2.204).toFixed(2);
+                
             }
         })
     
